4ae50f65c7773fcb9b4860ba8bab0a85e900601a
Chimmie Firefly <gameplayer2019pl@tutamail.com>
2025-01-10 17:51:29 +0000

    Patch name rotation.

--- scripts/list-images.sh
+++ scripts/list-images.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_PATH="$(dirname "$(realpath "$0")")"
 
-for IMAGE in $(ls "$SCRIPT_PATH/../dockerfiles" | rev | cut -f 1 -d '/' | cut -f 1 -d '-' | rev); do
+for IMAGE in $(ls "$SCRIPT_PATH/../dockerfiles" | rev | cut -f 1 -d '/' | rev | cut -f 2- -d '-'); do
     if ! echo "$IMAGE" | grep -q '\.offline'; then
         echo "$IMAGE"
     fi
