4befaf5f00bec7bbfbaba49b6d8027fa8ca18ccc
Chimmie Firefly <gameplayer2019pl@tutamail.com>
2025-01-10 13:07:34 +0000

    disable unusable images

--- scripts/list-images.sh
+++ scripts/list-images.sh
@@ -3,5 +3,7 @@
 SCRIPT_PATH="$(dirname "$(realpath "$0")")"
 
 for IMAGE in $(ls "$SCRIPT_PATH/../dockerfiles" | rev | cut -f 1 -d '/' | cut -f 1 -d '-' | rev); do
-    echo "$IMAGE"
+    if ! echo "$IMAGE" | grep -q '\.offline'; then
+        echo "$IMAGE"
+    fi
 done | tr '\n' ' '
