2a4c6558e3abe974842cea300fa105a5a994d47a
Chimmie Firefly <gameplayer2019pl@tutamail.com>
2025-01-10 16:48:56 +0000

    Fix naming.

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -6,6 +6,6 @@ IMAGES="${IMAGES:-$*}"
 
 for IMAGE in $IMAGES; do
     docker build -t \
-        "$(echo "${IMAGE_NAME}" | cut -f 1 -d ':')-${IMAGE}:$(echo "${IMAGE_NAME}" | cut -f 1 -d ':')" \
+        "$(echo "${IMAGE_NAME}" | cut -f 1 -d ':')-${IMAGE}:$(echo "${IMAGE_NAME}" | cut -f 2 -d ':')" \
         -f dockerfiles/Dockerfile-"${IMAGE}" .
 done
--- scripts/publish-images.sh
+++ scripts/publish-images.sh
@@ -6,5 +6,5 @@ IMAGES="${IMAGES:-$*}"
 
 for IMAGE in $IMAGES; do
     docker push \
-        "$(echo "${IMAGE_NAME}" | cut -f 1 -d ':')-${IMAGE}:$(echo "${IMAGE_NAME}" | cut -f 1 -d ':')"
+        "$(echo "${IMAGE_NAME}" | cut -f 1 -d ':')-${IMAGE}:$(echo "${IMAGE_NAME}" | cut -f 2 -d ':')"
 done
